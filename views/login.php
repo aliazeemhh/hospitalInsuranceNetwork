cffb1b1d999121ac8832a0c22224ae760b9ce565
Ali Azeem <aliazeemh@gmail.com>
2019-05-16 01:31:06 +0500

    updates

--- views/login.php
+++ views/login.php
@@ -1,3 +1,61 @@
-<?php
-  echo "Success!!" 
-?>
+
+ <div class="page-wrapper">
+   <div class="left-pannel pannel-img">
+
+   </div>
+   <div class="right-pannel">
+     <header class="header">
+     </header>
+     <form class="login" action="#" method="post">
+       <input type="text" name="username"  placeholder="LOGIN ID">
+       <input type="password" name="password"  placeholder="PASSWORD">
+       <input type="submit" name="signin" value="SIGN IN">
+       <input type="checkbox" name="check" value="Stay Signed in">
+     </form>
+     <a href="#">Forgot Password?</a>
+   </div>
+ </div>
+ <?php
+ function checkData()
+ {
+   echo $_POST["username"];
+   $this->form();
+ }
+ ?>
+ <script type="text/javascript">
+ window.onload = function()
+ {
+   $(".login").submit(function(event)
+   {
+     event.preventDefault();
+     //console.log($(this).find("input[name='username']").val());
+     var form = $.extend(true,{},$(this).serialize().split("&"));
+     var temp = {};
+     $.each(form,function(key,data)
+      {
+        data = String(data).split("=");
+        temp[data[0]] = data[1];
+
+      });
+      var data = $(this).serialize();
+      console.log(data);
+      $.ajax({
+        type:"POST",
+        url:"controllers/submitQuery.php",
+        data:data,
+        cache:false,
+        success:function(response)
+        {
+          console.log(response);
+        }
+      })
+     //console.log(temp);
+     // var formData = new FormData(temp);
+     // var xhr = new XMLHttpRequest();
+     // xhr.open("POST", "controllers/submitQuery.php",true);
+     // console.log(formData);
+     // xhr.send(temp);
+   });
+
+ }
+ </script>
