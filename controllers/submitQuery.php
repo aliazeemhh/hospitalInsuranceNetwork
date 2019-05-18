50b7caf3b6b407dad1fd418b98e7b7eae583a352
Ali Azeem <aliazeemh@gmail.com>
2019-05-19 01:42:15 +0500

    login functionality completed

--- controllers/submitQuery.php
+++ controllers/submitQuery.php
@@ -1,7 +1,14 @@
 <?php
 namespace controllers;
 
-require '../models/db_get_data.php';
+if(isset($_POST['username']) && $_POST['password'])
+{
+  require '../models/db_insert_data.php';
+}
+else if(isset($_GET['username']) && $_GET['password'])
+{
+  require '../models/db_get_data.php';
+}
 
 use models\db_get_data as db_get_data;
 /**
@@ -12,12 +19,32 @@ class signup_login extends db_get_data
 
   function __construct()
   {
-    if(isset($_POST['username']) && $_POST['password'])
+    if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['name']) && isset($_POST['email_id']) && isset($_POST['role']) && isset($_POST['sub_role']))
     {
-      $querying = new db_get_data();
+      $querying = new db_insert_data();
       $querying->insertData();
     }
-    // code...
+    else if(isset($_POST['username']) && isset($_POST['password']))
+    {
+      $querying = new db_get_data();
+      $getData = $querying->checkLogin($_GET['username'],  $_GET['password']);
+      if($getData == "false")
+      {
+        $arr = ["status"=>"fail", "response"=>"user details was not found"];
+      }
+      else {
+        $userProfile = [];
+        foreach ($getData as $key => $value)
+        {
+          if(!is_numeric($key))
+          {
+            $userProfile[$key] = $value;
+          }
+        }
+        $arr = ["status"=>"success", "response"=>$userProfile];
+      }
+      echo json_encode($arr);
+    }
   }
 }
 $login = new signup_login();
--- models/db_connect.php
+++ models/db_connect.php
@@ -1,24 +1,16 @@
 <?php
-namespace models;
-
-class db_connect
+function OpenCon()
 {
-  function OpenCon()
-  {
-     echo "db_con.php OpenCon function called<br>";
-     $dbhost = "localhost";
-     $dbuser = "root";
-     $dbpass = "";
-     $db = "health_insurance_network";
-     $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
-     return $conn;
-  }
-
-  function CloseCon($conn)
-  {
-    $conn -> close();
-  }
+   $dbhost = "localhost";
+   $dbuser = "root";
+   $dbpass = "";
+   $db = "health_insurance_network";
+   $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+   return $conn;
 }
 
-
- ?>
+function CloseCon($conn)
+{
+  $conn -> close();
+}
+?>
